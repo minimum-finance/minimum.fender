7e0f79f3ceb5e352abd20fdfdc92e4941232e38f
owenThurm <owen.p.thurm@gmail.com>
2021-11-10 19:04:45 -0500

    Menu Layout bugfix

--- src/AppLayout.js
+++ src/AppLayout.js
@@ -38,8 +38,8 @@ const AppLayout = () => (
       <LogoContainer>
         <img src={logo} style={{maxWidth: '100%'}} alt="minimum.finance" />
       </LogoContainer>
-      <HeaderMenu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item class="fromCenter" key="1">nav 1</Menu.Item>
+      <HeaderMenu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+        <Menu.Item key="1">nav 1</Menu.Item>
         <Menu.Item key="2">nav 2</Menu.Item>
         <Menu.Item key="3">nav 3</Menu.Item>
       </HeaderMenu>
